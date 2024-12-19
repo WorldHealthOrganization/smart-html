ffb8de3eb8bb4b70cfbd39c6832ab9df1f11ea75
ritikarawlani <38657562+ritikarawlani@users.noreply.github.com>
2024-12-19 18:56:55 +0530

    Trust v1.1.5 release

--- trust/CodeSystem/WHO.TRUST.TRANSACTION/index.php
+++ trust/CodeSystem/WHO.TRUST.TRANSACTION/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.json2');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.json1');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.json');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.xml2');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.xml1');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.html');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.html');
 else 
-  Redirect('http://smart.who.int/trust/1.1.4/CodeSystem-WHO.TRUST.TRANSACTION.xml');
+  Redirect('http://smart.who.int/trust/v1.1.5/CodeSystem-WHO.TRUST.TRANSACTION.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
