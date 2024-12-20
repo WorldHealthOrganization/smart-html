da8f1c74fe8cf681342fa28af9240b6f1890ab03
ritikarawlani <38657562+ritikarawlani@users.noreply.github.com>
2024-12-20 12:01:17 +0530

    release trust v.1.1.5

--- trust/StructureDefinition/SmartHealthLink/index.php
+++ trust/StructureDefinition/SmartHealthLink/index.php
@@ -7,19 +7,19 @@ function Redirect($url)
 
 $accept = $_SERVER['HTTP_ACCEPT'];
 if (strpos($accept, 'application/json+fhir') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.json2');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.json2');
 elseif (strpos($accept, 'application/fhir+json') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.json1');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.json1');
 elseif (strpos($accept, 'json') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.json');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.json');
 elseif (strpos($accept, 'application/xml+fhir') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.xml2');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.xml2');
 elseif (strpos($accept, 'application/fhir+xml') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.xml1');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.xml1');
 elseif (strpos($accept, 'html') !== false)
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.html');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.html');
 else 
-  Redirect('http://smart.who.int/trust/1.1.4/StructureDefinition-SmartHealthLink.xml');
+  Redirect('http://smart.who.int/trust/v1.1.5/StructureDefinition-SmartHealthLink.xml');
 ?>
     
 You should not be seeing this page. If you do, PHP has failed badly.
